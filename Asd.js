01dd4254dcb42a7984e41342ddee85cacea00618
LeaderWithHelmet <120545223+LeaderWithHelmet@users.noreply.github.com>
2023-07-12 07:44:16 +0800

    Create Asd.js
    the first JS

---
+++ Asd.js
@@ -0,0 +1,28 @@
+var a = 1 / 3;
+var b = 2 / 3;
+var c_1 = "lore menry lass to fatr less"
+var c_2 = "Lorem ipsum dolor sit, amet consectetur adipisicing \
+    elit. Amet illum quaerat placeat quod necessitatibus\
+    vero eius officia. Ipsum corrupti facilis beatae \
+    eos deleniti laudantium culpa assumenda praesentium \
+    ut quis! Quam ipsam fugiat qui eaque mollitia \
+    dignissimos assumenda iure esse omnis est pariatur \
+    earum, eligendi \
+    velit perspiciatis placeat id illum molestiae?"
+window.alert(Number(a.toFixed(2)));
+function Fun_1(){
+    if(c_2 == document.getElementById("b3-p2").innerHTML){
+        return
+    }
+    document.getElementById("b3-p2").innerHTML=c_2
+}
+function Fun_2(){
+    if(c_1 == document.getElementById("b3-p2").innerHTML){
+        return
+    }
+    document.getElementById("b3-p2").innerHTML=c_1
+}
+function Fun_3(){
+    x = document.forms["Form"]["First"].
+    if()
+}
